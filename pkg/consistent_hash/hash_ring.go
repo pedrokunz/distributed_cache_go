67d2b102344cefcf5f871d00f30d322c5d826b83
Pedro Kunz <pedro.kunz@gmail.com>
2024-10-03 15:55:11 -0300

    refactor hash ring
    - renaming file
    - using the DataNode type

--- pkg/consistent_hash/hash_ring.go
+++ pkg/consistent_hash/hash_ring.go
@@ -1,32 +1,36 @@
 package consistent_hash
 
 import (
+	"github.com/pedrokunz/distributed_cache_go/internal/data_node"
 	"hash/crc32"
 	"sort"
 	"strconv"
 	"sync"
 )
 
+// HashRing is a consistent hash ring data structure
+// - It manages the consistent hashing logic
+// - It determines the node for a given key based on the hash
 type HashRing struct {
 	mu       sync.RWMutex
 	replicas int
 	keys     []int
-	hashMap  map[int]string
+	hashMap  map[int]*data_node.DataNode
 }
 
 func New(replicas int) *HashRing {
 	return &HashRing{
 		replicas: replicas,
-		hashMap:  make(map[int]string),
+		hashMap:  make(map[int]*data_node.DataNode),
 	}
 }
 
-func (hr *HashRing) AddNode(node string) {
+func (hr *HashRing) AddNode(node *data_node.DataNode) {
 	hr.mu.Lock()
 	defer hr.mu.Unlock()
 
 	for replicaIndex := 0; replicaIndex < hr.replicas; replicaIndex++ {
-		hashKey := hr.hashKey(replicaIndex, node)
+		hashKey := hr.hashKey(replicaIndex, node.ID())
 		hr.keys = append(hr.keys, hashKey)
 		hr.hashMap[hashKey] = node
 	}
@@ -34,12 +38,12 @@ func (hr *HashRing) AddNode(node string) {
 	sort.Ints(hr.keys)
 }
 
-func (hr *HashRing) RemoveNode(node string) {
+func (hr *HashRing) RemoveNode(node *data_node.DataNode) {
 	hr.mu.Lock()
 	defer hr.mu.Unlock()
 
 	for replicaIndex := 0; replicaIndex < hr.replicas; replicaIndex++ {
-		hashKey := hr.hashKey(replicaIndex, node)
+		hashKey := hr.hashKey(replicaIndex, node.ID())
 		index := sort.SearchInts(hr.keys, hashKey)
 
 		keysBeforeIndex := hr.keys[:index]
@@ -50,15 +54,15 @@ func (hr *HashRing) RemoveNode(node string) {
 	}
 }
 
-func (hr *HashRing) GetNode(node string) string {
+func (hr *HashRing) GetNode(key string) *data_node.DataNode {
 	hr.mu.RLock()
 	defer hr.mu.RUnlock()
 
 	if len(hr.keys) == 0 {
-		return ""
+		return nil
 	}
 
-	hash := hr.hashKey(0, node)
+	hash := hr.hashKey(0, key)
 	index := sort.Search(len(hr.keys), func(i int) bool {
 		return hr.keys[i] >= hash
 	})
@@ -70,7 +74,19 @@ func (hr *HashRing) GetNode(node string) string {
 	return hr.hashMap[hr.keys[index]]
 }
 
-func (hr *HashRing) hashKey(replicaIndex int, node string) int {
-	key := strconv.Itoa(replicaIndex) + node
+func (hr *HashRing) Nodes() []*data_node.DataNode {
+	hr.mu.RLock()
+	defer hr.mu.RUnlock()
+
+	nodes := make([]*data_node.DataNode, 0, len(hr.hashMap))
+	for _, node := range hr.hashMap {
+		nodes = append(nodes, node)
+	}
+
+	return nodes
+}
+
+func (hr *HashRing) hashKey(replicaIndex int, nodeID string) int {
+	key := strconv.Itoa(replicaIndex) + nodeID
 	return int(crc32.ChecksumIEEE([]byte(key)))
 }
