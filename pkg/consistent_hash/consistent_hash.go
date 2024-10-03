6dc2399b9f72cab35bd9ae27980f1599ea770ae6
Pedro Kunz <pedro.kunz@gmail.com>
2024-10-03 13:23:33 -0300

    Refactor HashRing to support adding and removing nodes
    - add support for adding and removing nodes.
    - additionally, a new helper function, hashKey, has been added to calculate the hash key for a given replica index and node.

--- pkg/consistent_hash/consistent_hash.go
+++ pkg/consistent_hash/consistent_hash.go
@@ -1,6 +1,11 @@
 package consistent_hash
 
-import "sync"
+import (
+	"hash/crc32"
+	"sort"
+	"strconv"
+	"sync"
+)
 
 type HashRing struct {
 	mu       sync.RWMutex
@@ -14,4 +19,38 @@ func New(replicas int) *HashRing {
 		replicas: replicas,
 		hashMap:  make(map[int]string),
 	}
+}
+
+func (hr *HashRing) AddNode(node string) {
+	hr.mu.Lock()
+	defer hr.mu.Unlock()
+
+	for replicaIndex := 0; replicaIndex < hr.replicas; replicaIndex++ {
+		hashKey := hr.hashKey(replicaIndex, node)
+		hr.keys = append(hr.keys, hashKey)
+		hr.hashMap[hashKey] = node
+	}
+
+	sort.Ints(hr.keys)
+}
+
+func (hr *HashRing) RemoveNode(node string) {
+	hr.mu.Lock()
+	defer hr.mu.Unlock()
+
+	for replicaIndex := 0; replicaIndex < hr.replicas; replicaIndex++ {
+		hashKey := hr.hashKey(replicaIndex, node)
+		index := sort.SearchInts(hr.keys, hashKey)
+
+		keysBeforeIndex := hr.keys[:index]
+		keysAfterIndex := hr.keys[index+1:]
+		hr.keys = append(keysBeforeIndex, keysAfterIndex...)
+
+		delete(hr.hashMap, hashKey)
+	}
+}
+
+func (hr *HashRing) hashKey(replicaIndex int, node string) int {
+	key := strconv.Itoa(replicaIndex) + node
+	return int(crc32.ChecksumIEEE([]byte(key)))
 }
